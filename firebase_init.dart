d54d56825ca652b9774ae4c658267e375dc72ad0
shashank5shekhar-sys <shashank5.shekhar@paytmservices.com>
2026-04-23 13:19:59 +0530

    Update firebase_init.dart

--- firebase_init.dart
+++ firebase_init.dart
@@ -5,8 +5,8 @@ class SupabaseInit {
   static Future<void> initialize() async {
     try {
       await Supabase.initialize(
-        url: 'YOUR_SUPABASE_URL',        // 👈 from Supabase dashboard
-        anonKey: 'YOUR_SUPABASE_ANON_KEY', // 👈 from Supabase dashboard
+        url: 'https://supabase.com/dashboard/project/okgybtvfsvsblbkibqun',        // 👈 from Supabase dashboard
+        anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZ3lidHZmc3ZzYmxia2licXVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MjMwNjcsImV4cCI6MjA5MjQ5OTA2N30.iSpM13qBaFvC5Ax8rPNgCL9zdKTfKk5VojqN_zQ88xE', // 👈 from Supabase dashboard
       );
     } catch (e) {
       if (kDebugMode) {
